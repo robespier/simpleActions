199260b71bc39aa08fe814fb91bcf05e5f8d9081
robespier <robespier@rambler.ru>
2019-05-14 13:17:52 +0400

    extended functionality Unclipping Mask
    Добавлена поддержка таких типов как CompoundPath, CompoundShape и TextItem в качестве контура маски.

--- jsx/UnclippingMask.jsx
+++ jsx/UnclippingMask.jsx
@@ -1,27 +1,79 @@
+﻿// Unclipping Mask
+// Script to extend the functionality of the Clipping Mask in Adobe Illustrator
+// version: 2.0.0
+// release: May 2019
+// author: Roman Sergeev
+// mail: robespier@rambler.ru | skype: romaya7 | github: robespier
 #target illustrator
 if (app.documents.length > 0) {
   var doc = app.activeDocument;
   var sel = doc.selection;
+  var clipPath = null;
   if (sel.length > 0) {
     if (sel[0].typename == 'GroupItem' && sel[0].clipped == true) {
-      var clipPath = null;
       var clipGroup = sel[0].pageItems.length;
         for (var i = 0; i < clipGroup; i++) {
           if (sel[0].pageItems[i].typename == 'PathItem' && sel[0].pageItems[i].clipping == true) {
             clipPath = sel[0].pageItems[i];
+            break;
           };
         };
       if (clipPath == null) {
+        var clipGroup = sel[0].pageItems.length;
         for (var i = 0; i < clipGroup; i++) {
           if (sel[0].pageItems[i].typename == 'CompoundPathItem' && sel[0].pageItems[i].pathItems[0].clipping == true) {
             clipPath = sel[0].pageItems[i];
+            break;
           }
         };
       };
       if (clipPath == null) {
+        var clipGroup = sel[0].pageItems.length;
         for (var i = 0; i < clipGroup; i++) {
           if (sel[0].pageItems[i].typename == 'PluginItem') {
-            clipPath = sel[0].pageItems[i];
+            sel[0].pageItems[i].remove();
+            app.redraw();
+            if (sel[0].clipped == false) {
+             app.undo();
+             clipPath = sel[0].pageItems[i];
+             break;
+            }
+            else {
+              app.undo();
+            }
+          };
+        };
+      };
+      if (clipPath == null) {
+        var clipGroup = sel[0].pageItems.length;
+        for (var i = 0; i < clipGroup; i++) {
+          if (sel[0].pageItems[i].typename == 'TextFrame' && sel[0].pageItems[i].kind == 'TextType.POINTTEXT') {
+            sel[0].pageItems[i].convertPointObjectToAreaObject();
+            app.redraw();
+            if (sel[0].clipped == false) {
+             app.undo();
+             clipPath = sel[0].pageItems[i];
+             break;
+            }
+            else {
+              app.undo();
+            }
+          }
+        };
+      };
+      if (clipPath == null) {
+        var clipGroup = sel[0].pageItems.length;
+        for (var i = 0; i < clipGroup; i++) {
+          if (sel[0].pageItems[i].typename == 'TextFrame' && sel[0].pageItems[i].kind == 'TextType.PATHTEXT') {
+            sel[0].pageItems[i].convertPointObjectToAreaObject();
+            app.redraw();
+            if (sel[0].clipped == true) {
+             clipPath = sel[0].pageItems[i];
+             break;
+            }
+            else {
+              app.undo();
+            }
           }
         };
       };
@@ -29,13 +81,13 @@ if (app.documents.length > 0) {
       clipPath.remove();
     }
     else {
-      alert ('Выделение не является объектом-маской!');
+      alert ('Выделение не является объектом-маской!\nSelection is not a mask object!');
     };
   }
   else {
-    alert ('Нет выделенных объектов!');
+    alert ('Нет выделенных объектов!\nNo objects selected!');
   };
 }
 else {
-  alert('Нет открытых документов!');
+  alert ('Нет открытых документов!\nNo open documents!');
 };
